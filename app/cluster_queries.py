dc8cea9bd8c3606eb39ca5b0b0d1134fef0a6df7
Utsav Talwar <utsav.talwar@mongodb.com>
2023-03-03 01:00:50 +0530

    Optimized

--- app/cluster_queries.py
+++ app/cluster_queries.py
@@ -1,59 +1,57 @@
 import pymongo
 import json
 from bson import json_util
-import app.logger as log
-
-logging = log.setup_logger()
+import pandas as pd
 
 class MongoAtlasClient:
     
-    def __init__(self, uri, database, collection_name):
+    def __init__(self, uri, database, collection_name, bucket_name):
         self.client = pymongo.MongoClient(uri)
         self.db = self.client[database]
         self.coll = self.db[collection_name]
-        self.json_path = 'app/temp_data_store/json_data'
+        self.bucket_name = bucket_name
 
-
-    def find_records_by_field(self, query_dict):
+    def find_records_by_field(self, query_dict, s3_resource):
         """
         Finds all documents in the collection that match a given query.
 
         Args:
             query_dict (dict): A dictionary representing the query to run.
-
-        Returns:
-            int: The number of documents found.
         """
-        documents_found = self.coll.find(query_dict)
-        self.split_into_json(documents_found)
-        return 1
+        try:
+            documents_found = self.coll.find(query_dict)
+            
+            for i, document in enumerate(documents_found):
+                json_data = json.loads(json_util.dumps(document))
+                # Create parquet file from json
+                df = pd.DataFrame(json_data, index=[0])
+                parquet = df.to_parquet()
+                # Upload to S3
+                isUploaded = self.archive_record(parquet, s3_resource, document['st'])
+                # Delete file from cluster
+                if isUploaded:
+                    self.delete_records_by_field(query_dict)
+        except Exception as e:
+            print(f"exception occured {e}")
 
-
-    def split_into_json(self, cursor):
-        """
-        Split a MongoDB cursor into individual JSON files and write them to disk.
-
-        Args:
-            cursor (pymongo.cursor.Cursor): The cursor to split into individual
-                documents.
-
-        Returns:
-            None
-        """
-        for i, document in enumerate(cursor):
-            # Construct the file name using the document's _id field
-            file_name = f"{self.json_path}/{document['st']}.json"
-
-            # Write the document to a JSON file
-            with open(file_name, 'w') as f:
-                json.dump(json.loads(json_util.dumps(document)), f)
-
-            logging.info(f"Exported document {i+1} to {file_name}")
-
+    def archive_record(self, parquet, s3_resource, doc_id):
+        success = False
+        try:
+            s3_resource.Bucket(self.bucket_name).put_object(Body=parquet, Key=f"archive/{doc_id}.parquet")
+            s3_resource.Object(self.bucket_name, f"archive/{doc_id}.parquet").wait_until_exists()
+            print("uploaded to s3")
+            success = True
+            
+        except Exception as e:
+            print(f"exception - {e}")
+        
+        finally:
+            return success
 
     def delete_records_by_field(self, query_dict):
         try:
+            print("deleting records from cluster")
             return self.coll.delete_many(query_dict)
         except Exception as e:
-            logging.exception("Exception in delete records : ", e)
+            print("Exception in delete records : ", e)
             return None
--- main.py
+++ main.py
@@ -1,54 +1,36 @@
 import app.cluster_queries as cq
-import app.archival_storage as arch
 import schedule
 import time
 import datetime
-import app.logger as log
+import boto3
 import config
 
-logging = log.setup_logger()
+# logging = log.setup_logger()
 
 def run_archival():
     try:
         # Data federation URI
-        uri = "mongodb://admin:admin@weatherarchive-fmxyq.a.query.mongodb.net/?ssl=true&authSource=admin"
-        query_dict = {'type' : "FM-13"}
+        uri = f"mongodb+srv://{config.user}:{config.password}@{config.server_addr}.mongodb.net/?retryWrites=true&w=majority"
+        query_dict = {'st': "x+45200-066500"}
 
         # Create a client object to connect to the MongoDB database
-        client = cq.MongoAtlasClient(uri, 'sample', 'weather')
-        logging.info("fetching records for that match query")
-        client.find_records_by_field(query_dict)
-
-        # Convert the retrieved records to the Parquet format
-        logging.info("converting to parquet")
-        archive_client = arch.ArchivalClient(
-                aws_access_key_id=config.AWS_ACCESS_KEY_ID, 
-                aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
-                aws_session_token=config.AWS_SESSION_TOKEN
-            )
+        client = cq.MongoAtlasClient(uri, collection_name='sample_weatherdata', database='data', bucket_name='partner-demo')
         
-        archive_client.convert_to_parquet()
-
-        # Upload the Parquet file to an S3 bucket using AWS credentials
-        archive_client.upload_to_S3()
+        s3_resource = boto3.resource(
+            "s3",
+            region_name="us-east-1",
+            aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
+            aws_session_token=config.AWS_SESSION_TOKEN
+        )
         
-        logging.info("deleting archived records from hot data")
-        # client.delete_records_by_field(query_dict)
+        print("fetching records for that match query")
+        client.find_records_by_field(query_dict, s3_resource)
 
     except Exception as e:
-        logging.exception(e)
+        print(e)
 
 
 if __name__ == "__main__":
-
-    logging.info("----------------started archival process----------------")
-
+    print("----------------started archival process----------------")
     run_archival()
-    # Schedule for every 1 minute
-    # schedule.every(1).minutes.do(run_archival())
-    
-    # while True:
-    #     # Checks whether a scheduled task
-    #     # is pending to run or not
-    #     schedule.run_pending()
-    #     time.sleep(1)
